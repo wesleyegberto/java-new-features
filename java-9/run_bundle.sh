e468790d30b5ae0df8ce9a759363d442fdbd47af
Wesley Egberto <wesleyegberto@gmail.com>
2017-04-24 01:24:18 -0300

    Java 9: added example to run generated script

--- java-9/run_bundle.sh
+++ java-9/run_bundle.sh
@@ -1,2 +1,5 @@
 # To run we use the java bin generated passing the module to run
 ./output/simplecalculator/bin/java -m simplecalculator
+
+# running the generated script
+./output/simplecalculator/bin/simplecalculator
