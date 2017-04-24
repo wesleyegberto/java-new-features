3dff29a13b403f112591dfd7943aeeb93b0c9413
Wesley Egberto <wesleyegberto@gmail.com>
2017-04-24 01:36:34 -0300

    Java 9: added listing modules

--- java-9/run_bundle.sh
+++ java-9/run_bundle.sh
@@ -1,3 +1,6 @@
+# listing the modules
+./output/simplecalculator/bin/java --list-modules
+
 # To run we use the java bin generated passing the module to run
 ./output/simplecalculator/bin/java -m simplecalculator
 
