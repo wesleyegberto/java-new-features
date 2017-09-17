ccb9b5a962c0cf8e4e81453b4cdf4d939c014d1f
Wesley Egberto <wesleyegberto@gmail.com>
2017-09-17 13:13:36 -0300

    echos

--- java-9/modules/run_bundle.sh
+++ java-9/modules/run_bundle.sh
@@ -1,4 +1,5 @@
 # listing the modules
+echo "Modules in the bundle:"
 ./output/simplecalculator/bin/java --list-modules
 
 # To run we use the java bin generated passing the module to run
--- java-9/modules/show_deps.sh
+++ java-9/modules/show_deps.sh
@@ -1,4 +1,9 @@
+echo "MathApi deps:"
 $JAVA9_HOME/bin/jdeps output/mlibs/mathapi-1.0.jar
+
+echo "\nMathLib deps:"
 # we need to set modules path if a module use any other define
 $JAVA9_HOME/bin/jdeps --module-path output/mlibs/ output/mlibs/mathlib-1.0.jar
+
+echo "\nCalculator deps:"
 $JAVA9_HOME/bin/jdeps --module-path output/mlibs/ output/mlibs/calculator.jar
