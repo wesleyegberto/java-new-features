d5ab3e34ce12e9497befac135b01ec10d026d7d1
Wesley Egberto <wesleyegberto@gmail.com>
2020-04-07 22:37:13 -0300

    Java 14: new escape sequences in text blocks

---
+++ java-14/TextBlocksExample.java
@@ -0,0 +1,51 @@
+public class TextBlocksExample {
+	public static void main(String[] args) {
+		// Java 13
+
+		// Error (same line)
+		// String name = """Pat Q. Smith""";
+
+		// 3 double-quote must be followed by line terminator (new line)
+		String niceJson = """
+		{
+			"name": "Odair Jose",
+			"language": "Java"
+		}"""; // same line to not append \n at the end
+		System.out.println(niceJson);
+
+		// the identation is kept based on the most left char of block (incidental white spaces)
+		String html = """
+		<html>
+		    <body>
+		        <p>Hello World.</p>
+		    </body>
+		</html>
+		"""; // will append a \n at the end
+		System.out.println(html);
+
+		String badJson = """
+				{
+			"name": "Odair Jose",
+			"language": "Java"
+				}""";
+		System.out.println(badJson);
+
+		
+		// Java 14 - New escape sequences
+
+		String sameLine = """
+						  Line 1 \
+						  Still line 1 \
+						  Still... \
+						  """;
+		System.out.println(sameLine);
+
+		String helpingKeepingTrailing = """
+				A lot of space here               \s
+				Here not much                     \s
+				Here none but one will be inserted\s
+				Ends                              \s
+				""";
+		System.out.println(helpingKeepingTrailing);
+	}
+}
