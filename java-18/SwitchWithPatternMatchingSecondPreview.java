aea06244fe3c081aff207e9dd673d9f90af42e97
Wesley Egberto <wesleyegberto@gmail.com>
2021-11-27 15:49:47 -0300

    Java 18: started example of switch preview 2

---
+++ java-18/SwitchWithPatternMatchingSecondPreview.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Stream;
+
+/**
+ * Run: `java --enable-preview --source 18 <FileName.java>`
+ */
+public class SwitchWithPatternMatchingSecondPreview {
+	public static void main(String[] args) {
+		System.out.println(stringify(42));
+		System.out.println(stringify(-42));
+		System.out.println(stringify("Some text"));
+		System.out.println(stringify(""));
+	}
+
+	static String stringify(Object value) {
+		return switch (value) {
+			// the constant must be before the guarded pattern (otherwise it will never hit)
+			case 42 -> "42 is the answer";
+			case Integer i && i < 0 -> "negative number";
+			// this must be after because it will match all integers
+			case Integer i -> "some number";
+
+			case String s && s.isEmpty() -> "empty string";
+			case String s && s.length() > 50 -> "long string";
+			// this must be after because it will match all strings
+			case String s -> "non-empty string";
+
+			default -> "unhandled type";
+		};
+	}
+}
